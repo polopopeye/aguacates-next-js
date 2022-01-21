180629608730b6da7a9777ab8293c5d20de68dd2
Kenneth Suarez <33601506+polopopeye@users.noreply.github.com>
2022-01-21 01:58:16 +0100

    feat: home and product page ini

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
+  const [productList, setProductList] = useState([])
+
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({ data, length }) => {
+        // console.log(data)
+
+        setProductList(data)
+      })
+  }, [])
+
   return (
     <div>
       <Navbar />
       <div>Platzi and Next.js!</div>
+      {productList.map((product) => (
+        <>
+          <div>{product.image} </div>
+          <div>{product.name} </div>
+        </>
+      ))}
     </div>
   )
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
   const { query } = useRouter()
+  const [product, setProduct] = useState([])
+
+  const productId = query.id
+
+  useEffect(() => {
+    window
+      .fetch('/api/avo/' + productId)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log({ data })
+
+        setProduct(data)
+      })
+  }, [])
 
   return (
     <section>
       <h1>Página producto: {query.id}</h1>
+
+      <div>{product.image}</div>
+      <div>{product.name}</div>
     </section>
   )
 }
